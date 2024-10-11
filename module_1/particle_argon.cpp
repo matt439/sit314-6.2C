a1d191b344900301c41b4cf9336fb441ab0343e4
matt439 <120173618+matt439@users.noreply.github.com>
2024-10-11 12:34:34 +1000

    change formatting

--- module_1/particle_argon.cpp
+++ module_1/particle_argon.cpp
@@ -2,13 +2,13 @@
 BH1750 sensor(0x23, Wire);
 void setup()
 {
-sensor.begin();
-sensor.set_sensor_mode(BH1750::forced_mode_high_res2);
-Serial.begin();
+    sensor.begin();
+    sensor.set_sensor_mode(BH1750::forced_mode_high_res2);
+    Serial.begin();
 }
 void loop()
 {
-sensor.make_forced_measurement();
-Particle.publish("lux-sensor", String(sensor.get_light_level()), PRIVATE);
-delay(10000);
+    sensor.make_forced_measurement();
+    Particle.publish("lux-sensor", String(sensor.get_light_level()), PRIVATE);
+    delay(10000);
 }
